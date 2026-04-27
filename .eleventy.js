a4378798fc451258438f04519511f536f6ebd47e
Farooq Mohammed <farooq@Farooqs-MacBook-Air-2.local>
2026-04-27 11:54:50 -0700

    Publish first set of ghazals

--- .eleventy.js
+++ .eleventy.js
@@ -5,14 +5,27 @@ module.exports = function(eleventyConfig) {
   // Watch for asset changes
   eleventyConfig.addWatchTarget("src/assets");
   
-  // Ghazals collection
-  eleventyConfig.addCollection("ghazals", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("src/content/ghazals/*.json");
+  const fs = require('fs');
+  const path = require('path');
+  
+  // Custom filter to read ghazals JSON directly
+  eleventyConfig.addFilter("ghazals", function() {
+    const ghazalsDir = path.join(__dirname, 'src/content/ghazals');
+    const files = fs.readdirSync(ghazalsDir).filter(f => f.endsWith('.json'));
+    return files.map(f => {
+      const data = JSON.parse(fs.readFileSync(path.join(ghazalsDir, f), 'utf8'));
+      return { data: data };
+    });
   });
   
-  // Nazms collection
-  eleventyConfig.addCollection("nazms", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("src/content/nazms/*.json");
+  // Custom filter to read nazms JSON directly
+  eleventyConfig.addFilter("nazms", function() {
+    const nazmsDir = path.join(__dirname, 'src/content/nazms');
+    const files = fs.readdirSync(nazmsDir).filter(f => f.endsWith('.json'));
+    return files.map(f => {
+      const data = JSON.parse(fs.readFileSync(path.join(nazmsDir, f), 'utf8'));
+      return { data: data };
+    });
   });
   
   // Urdu date filter
