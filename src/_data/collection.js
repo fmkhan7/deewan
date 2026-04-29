2d82c2a0c39a4c1f6cb7a794b924c32f3118eb0c
Farooq Mohammed <farooq@Farooqs-MacBook-Air-2.local>
2026-04-29 04:19:25 -0700

    Revert multiple collection

--- src/_data/collection.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  eleventyComputed: {
-    collection: (data) => {
-      const collections = data.poetry_collections || [];
-      const activeSlug = data.site.activeCollection || 'deewan-e-ekta';
-      return collections.find(c => c.slug === activeSlug) || collections[0] || {};
-    }
-  }
-};
